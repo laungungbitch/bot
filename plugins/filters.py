78b1588ce8597d8cf6ff275f20ee490673ca04e1
laungungbitch <73893865+laungungbitch@users.noreply.github.com>
2021-04-18 07:40:54 +0700

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -78,8 +78,7 @@ async def filter(client: Bot, message: Message):
         )
 
         await message.reply_text(
-                f"<b> Kết quả tìm kiếm cho từ khóa: {message.text}
-                Join vào Channel trước mới xem được nhé </b>",
+                f"<b> Kết quả tìm kiếm cho từ khóa: {message.text}</b>",
                 reply_markup=InlineKeyboardMarkup(buttons)
             )    
 
